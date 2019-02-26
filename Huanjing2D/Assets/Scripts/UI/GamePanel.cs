fba85625b93193c3b1c1c722cdd2f6b89cbc9819
xiaoya6 <2310058944@qq.com>
2019-02-26 23:04:53 +0800

    UI
    主页界面和游戏界面布局，部分代码编写

---
+++ Huanjing2D/Assets/Scripts/UI/GamePanel.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GamePanel : MonoBehaviour {
+
+    private Button btn_Pause;
+    private Button btn_Play;
+    private Text txt_Scroe;
+    private Text txt_DiamondCount;
+
+    private void Awake()
+    {
+        Init();
+    }
+
+    private void Init()
+    {
+        EventCenter.AddListener(EventDefine.ShowGamePanel, Show);
+        btn_Pause = GameObject.Find("btn_Pause").GetComponent<Button>();
+        btn_Pause.onClick.AddListener(OnPauseButtonClick);
+        btn_Play = GameObject.Find("btn_Play").GetComponent<Button>();
+        btn_Play.onClick.AddListener(OnPlayButtonClick);
+        txt_DiamondCount = GameObject.Find("Diamond/txt_DiamondCount").GetComponent<Text>();
+        txt_Scroe = GameObject.Find("txt_Score").GetComponent<Text>();
+        gameObject.SetActive(false);
+        btn_Play.gameObject.SetActive(false);
+    }
+
+    private void OnDestroy()
+    {
+        EventCenter.RemoveListener(EventDefine.ShowGamePanel, Show);
+    }
+
+    private void Show() {
+        gameObject.SetActive(true);
+    }
+
+    private void OnPauseButtonClick() {
+        btn_Pause.gameObject.SetActive(false);
+        btn_Play.gameObject.SetActive(true);
+    }
+
+    private void OnPlayButtonClick() {
+        btn_Pause.gameObject.SetActive(true);
+        btn_Play.gameObject.SetActive(false);
+    }
+
+}
---
+++ Huanjing2D/Assets/Scripts/UI/MainPanel.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class MainPanel : MonoBehaviour {
+
+    private Button btn_Start;
+    private Button btn_Shop;
+    private Button btn_Rank;
+    private Button btn_Sound;
+
+    private void Awake()
+    {
+        Init();
+    }
+
+    private void Init() {
+        btn_Start = GameObject.Find("Btn_start").GetComponent<Button>();
+        btn_Start.onClick.AddListener(OnStartButtonCilck);
+        btn_Rank = GameObject.Find("Btns/Btn_rank").GetComponent<Button>();
+        btn_Rank.onClick.AddListener(OnRankButtonClick);
+        btn_Sound = GameObject.Find("Btns/Btn_sound").GetComponent<Button>();
+        btn_Sound.onClick.AddListener(OnSoundButtonClick);
+        btn_Shop = GameObject.Find("Btns/Btn_shop").GetComponent<Button>();
+        btn_Shop.onClick.AddListener(OnShopButtonClick);
+    }
+
+    private void OnStartButtonCilck() {
+        EventCenter.Broadcast(EventDefine.ShowGamePanel);
+        gameObject.SetActive(false);
+    }
+
+    private void OnShopButtonClick() {
+
+    }
+
+    private void OnRankButtonClick()
+    {
+
+    }
+
+    private void OnSoundButtonClick()
+    {
+
+    }
+}
